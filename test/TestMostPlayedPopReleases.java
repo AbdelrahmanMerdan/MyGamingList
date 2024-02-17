908256b8146e0194ae0b8bd79949f60d9237a503
Rocco Fernando Gadista <158077976+roccog-yorku@users.noreply.github.com>
2024-02-16 19:23:41 -0500

    Created TestMostReplayedPopReleases

---
+++ test/TestMostPlayedPopReleases.java
@@ -0,0 +1,23 @@
+package test;
+
+import src.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestMostPlayedPopReleases {
+
+	@Test
+	void test() {
+		MostPlayed mostPlayed = new MostPlayed();
+		assertEquals(100, mostPlayed.getSize());
+	}
+	
+	@Test
+	void test2() {
+		PopReleases popReleases= new PopReleases();
+		assertEquals(30, popReleases.getSize());
+	}
+
+}
