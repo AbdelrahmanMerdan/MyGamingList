7875497d9eeebc7b49009c687d07661e89082201
Rocco Fernando Gadista <158077976+219714732@users.noreply.github.com>
2024-02-25 18:33:57 -0500

    Update TestDatabase.java
    Fixed ordering so the game being tested is up-to-date

--- test/TestDatabase.java
+++ test/TestDatabase.java
@@ -2,8 +2,6 @@ package test;
 
 import static com.mongodb.client.model.Filters.eq;
 import static org.junit.jupiter.api.Assertions.*;
-
-import com.mongodb.client.result.DeleteResult;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -15,64 +13,67 @@ import org.junit.jupiter.api.TestMethodOrder;
 import com.mongodb.MongoException;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
 
 import database.Database;
 import src.Game;
 
-@Nested
 @TestMethodOrder(OrderAnnotation.class)
 class TestDatabase {
+	
 	private static final int TEST_GAME_ID = 1693980;
+	
+	@Nested
+	@TestMethodOrder(OrderAnnotation.class)
+	class TestGameDetail{
+		@BeforeEach
+		void deleteAppDetails() {
+			//Grabbing specified game
+			Bson filter = eq("_id", TEST_GAME_ID);
+			FindIterable<Document> result = Database.games.find(filter);
+			Document game = result.first();
+			Bson update = Updates.unset("description");
+			Bson update2 = Updates.unset("number_of_reviews");
+			Bson update3 = Updates.unset("user_reviews");
+			Bson total = Updates.combine(update, update2, update3);
 
-	@BeforeEach
-	void deleteAppDetails() {
-		//Grabbing specified game
-		Bson filter = eq("_id", TEST_GAME_ID);
-		FindIterable<Document> result = Database.games.find(filter);
-		Document game = result.first();
-		Bson update = Updates.unset("description");
-		Bson update2 = Updates.unset("number_of_reviews");
-		Bson update3 = Updates.unset("user_reviews");
-		Bson total = Updates.combine(update, update2, update3);
+			//Deleting description
+			try {
+				UpdateResult updateResult = Database.games.updateOne(game, total);
+				System.out.println("Acknowledged: "+updateResult.wasAcknowledged());
 
-		//Deleting description
-		try {
-			UpdateResult updateResult = Database.games.updateOne(game, total);
-			System.out.println("Acknowledged: "+updateResult.wasAcknowledged());
+			} catch(MongoException e) {
+				System.err.println("ERROR: "+e);
+			}
+		}
 
-		} catch(MongoException e) {
-			System.err.println("ERROR: "+e);
+		@Test
+		@Order(1)
+		void noAppDetails() {
+			assertEquals(true, Database.noAppDetails(TEST_GAME_ID));
+		}
+
+		@Test
+		@Order(2)
+		void noAppReviews() {
+			assertEquals(true, Database.noAppReviews(TEST_GAME_ID));
+		}
+
+		@Test
+		@Order(3)
+		void updateGame() {
+			Database.updateAppDetails(1693980);
+			assertEquals(false, Database.noAppDetails(TEST_GAME_ID));
+			assertEquals(false, Database.noAppReviews(TEST_GAME_ID));
 		}
 	}
-
+	
 	@Test
 	@Order(1)
-	void noAppDetails() {
-		assertTrue(Database.noAppDetails(TEST_GAME_ID));
-	}
-
-	@Test
-	@Order(2)
-	void noAppReviews() {
-		assertTrue(Database.noAppReviews(TEST_GAME_ID));
-	}
-
-	@Test
-	@Order(3)
-	void updateGame() {
-		Database.updateAppDetails(TEST_GAME_ID);
-		assertFalse(Database.noAppDetails(TEST_GAME_ID));
-		assertFalse(Database.noAppReviews(TEST_GAME_ID));
-	}
-
-	@Test
-	@Order(4)
 	void addGame() {
 		Bson filter = eq("_id", TEST_GAME_ID);
-		FindIterable<Document> result = Database.games.find(filter);
-		Document game = result.first();
-
+		
 		//Deleting game in database
 		try {
 			DeleteResult deleteResult= Database.games.deleteOne(filter);
@@ -90,34 +91,32 @@ class TestDatabase {
 		assertFalse(Database.noAppDetails(TEST_GAME_ID));
 		assertFalse(Database.noAppReviews(TEST_GAME_ID));
 	}
-
-	@Test
-	@Order(5)
-	void getGame() {
-		Game game = Database.getGame(TEST_GAME_ID);
-		assertNotNull(game);
-
-		String name = game.getName();
-		String desc = game.getDesc();
-		String sysReq = game.getSysRequire();
-		String metaUrl = game.getMetaURL();
-		String metaScore = game.getMetaScore();
-		String cover = game.getCover();
-		String shortDesc = game.getShortDesc();
-		Integer numOfReviews = game.getNumOfReviews();
-		Integer userReviews = game.getUserReviews();
-		assertNotNull(name);
-		assertNotNull(sysReq);
-		assertNotNull(metaScore);
-		assertNotNull(metaUrl);
-		assertNotNull(cover);
-		assertNotNull(shortDesc);
-		assertNotNull(numOfReviews);
-		assertNotNull(userReviews);
-		// desc was updated to be null in @BeforeEach
-		assertNull(desc);
-
-		assertEquals("Dead Space", name);
-	}
 	
+	@Test
+	@Order(2)
+	void getGame() {
+			Game game = Database.getGame(TEST_GAME_ID);
+			assertNotNull(game);
+
+			String name = game.getName();
+			String desc = game.getDesc();
+			String sysReq = game.getSysRequire();
+			String metaUrl = game.getMetaURL();
+			String metaScore = game.getMetaScore();
+			String cover = game.getCover();
+			String shortDesc = game.getShortDesc();
+			Integer numOfReviews = game.getNumOfReviews();
+			Integer userReviews = game.getUserReviews();
+			assertNotNull(name);
+			assertNotNull(sysReq);
+			assertNotNull(metaScore);
+			assertNotNull(metaUrl);
+			assertNotNull(cover);
+			assertNotNull(shortDesc);
+			assertNotNull(numOfReviews);
+			assertNotNull(userReviews);
+			assertNotNull(desc);
+
+			assertEquals("Dead Space", name);
+		}
 }
