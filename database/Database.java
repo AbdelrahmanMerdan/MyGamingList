be2497076c0fb0cdd995b3002744342fed9ad807
219714732 <roccog20@my.yorku.ca>
2024-03-09 21:19:39 -0500

    Refactoring Database to an interface, and added StubDatabase.java.

--- database/Database.java
+++ database/Database.java
@@ -1,25 +1,11 @@
 package database;
 
 import com.mongodb.*;
-import static com.mongodb.client.model.Filters.*;
 import com.mongodb.client.*;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.model.Updates;
-import com.mongodb.client.result.*;
-import org.bson.*;
-import org.bson.conversions.Bson;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.*;
-import src.Game;
 
-import java.net.*;
-import java.net.http.*;
-import java.io.IOException;
-
-
-public class Database {
-	//Client settings
-	final static String connection = "mongodb+srv://2311team5:qn431J86d7xMEdpc@mygaminglist.igshqok.mongodb.net/?retryWrites=true&w=majority&appName=MyGamingList";
+public interface Database {
+	//Client settings for real database
+	static String connection = "mongodb+srv://2311team5:qn431J86d7xMEdpc@mygaminglist.igshqok.mongodb.net/?retryWrites=true&w=majority&appName=MyGamingList";
 
 	final static ServerApi serverApi = ServerApi.builder()
 			.version(ServerApiVersion.V1)
@@ -32,6 +18,6 @@ public class Database {
 
 	final static MongoClient client = MongoClients.create(settings);
 
-	//Database
+	
 	final static MongoDatabase database = client.getDatabase("MyGamingList");
 }
