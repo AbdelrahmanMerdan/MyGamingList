98b3b1be15d8f392cf30a4707eeff9b13002bfd6
219714732 <roccog20@my.yorku.ca>
2024-03-09 19:16:47 -0500

    Update Game.java with hash & equals.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -102,6 +104,23 @@ public class Game {
 
 	public void setUserReviews(int userReviews) {
 		this.userReviews = userReviews;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return id == other.id;
 	}
 
 	@Override
