5ed014f7629c895efe12a4dcfe69de7949a06864
219714732 <roccog20@my.yorku.ca>
2024-03-09 22:52:05 -0500

    Refactoring

--- database/Database.java
+++ database/Database.java
@@ -5,7 +5,7 @@ import com.mongodb.client.*;
 
 public interface Database {
 	//Client settings for real database
-	static String connection = "mongodb+srv://2311team5:qn431J86d7xMEdpc@mygaminglist.igshqok.mongodb.net/?retryWrites=true&w=majority&appName=MyGamingList";
+	final static String connection = "mongodb+srv://2311team5:qn431J86d7xMEdpc@mygaminglist.igshqok.mongodb.net/?retryWrites=true&w=majority&appName=MyGamingList";
 
 	final static ServerApi serverApi = ServerApi.builder()
 			.version(ServerApiVersion.V1)
--- database/StubDatabase.java
+++ database/StubDatabase.java
@@ -9,8 +9,8 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 public interface StubDatabase {
-	//Client settings for real database
-	static String connection = "mongodb+srv://2311team5:qn431J86d7xMEdpc@gaminglist.a2jzcoi.mongodb.net/?retryWrites=true&w=majority&appName=GamingList";
+	//Client settings for stub database
+	final static String connection = "mongodb+srv://2311team5:qn431J86d7xMEdpc@gaminglist.a2jzcoi.mongodb.net/?retryWrites=true&w=majority&appName=GamingList";
 
 	final static ServerApi serverApi = ServerApi.builder()
 			.version(ServerApiVersion.V1)
