f50c3d06faac8b9fb4a3deac58e2a3624db02cef
AbdelrahmanMerdan <bodo010501@gmail.com>
2024-03-11 22:19:43 -0400

    Created Tests for Review Class

--- test/TestReview.java
+++ test/TestReview.java
@@ -2,15 +2,20 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
 
 import database.GameData;
+import database.StubGameData;
 import database.UsersImpl;
 
 import org.junit.jupiter.api.Test;
 
 import src.Game;
+import src.Review;
 import src.User;
 
 class TestReview {
@@ -21,20 +26,32 @@ class TestReview {
 
 	    private static final String TEST_USER = "test.user";
 	    private static final String TEST_PASSWORD = "test.password";
+	    Game game = GameData.getGame(17470);
 
 	    @BeforeEach
 	    public void setup() {
 	        user = new User(TEST_USER, TEST_PASSWORD);
 	        // insert a record
 	        users.createAccount(user);
-	        Game game = GameData.getGame(271590);
-	        
 	    }
 
 	    @AfterEach
 	    public void clean() {
 	        users.delete(TEST_USER);
-	        GameData.updateAppDetails(271590);
+	        GameData.updateAppDetails(17470);
 	    }
-	
+	    
+	    @Test
+		@Order(1)
+		void AlreadyReviewedTest() {
+			assertEquals(false, Review.AlreadyReviewed(game, TEST_USER));
+		}
+
+		@Test
+		@Order(2)
+		void ReviewGame() throws IOException {
+			Review.review_game(TEST_USER, game, 9, "Game is Good!", "Yes" );
+			assertEquals(true, Review.AlreadyReviewed(game, TEST_USER));
+		}
+
 }
