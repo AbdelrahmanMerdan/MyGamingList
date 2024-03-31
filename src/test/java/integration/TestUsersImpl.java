23ac9ca5771a1e859f9f849ac72907cd1705b266
youwei <youwei@my.yorku.ca>
2024-03-31 03:10:21 -0400

    Update account privacy after login

--- src/test/java/integration/TestUsersImpl.java
+++ src/test/java/integration/TestUsersImpl.java
@@ -19,8 +19,13 @@ public class TestUsersImpl {
     private static final String TEST_USER = "test.user";
     private static final String TEST_PASSWORD = "test.password";
 
+    private static final String TEST_USER_TO_PUBLIC = "test.userPrivateToPublic";
+    private static final String TEST_USER_TO_PRIVATE = "test.userPublicToPrivate";
+
     private static final String TEST_FRIEND_PRIVATE = "test.friendPrivate";
     private static final String TEST_FRIEND_PUBLIC = "test.friendPublic";
+
+
     private static final boolean TEST_IS_PRIVATE = true;
 
 
@@ -28,11 +33,15 @@ public class TestUsersImpl {
     @BeforeEach
     public void setup() {
         user = new User(TEST_USER, TEST_PASSWORD,TEST_IS_PRIVATE);
-
+        User privateToPublicAccount = new User(TEST_USER_TO_PUBLIC,TEST_PASSWORD,TEST_IS_PRIVATE);
+        User publicToPrivateAccount =  new User(TEST_USER_TO_PRIVATE,TEST_PASSWORD,false);
         User friendPrivateAccount = new User(TEST_FRIEND_PRIVATE,TEST_PASSWORD,TEST_IS_PRIVATE);
         User friendPublicAccount =  new User(TEST_FRIEND_PUBLIC,TEST_PASSWORD,false);
+
         // insert a record
         users.createAccount(user);
+        users.createAccount(privateToPublicAccount);
+        users.createAccount(publicToPrivateAccount);
         users.createAccount(friendPrivateAccount);
         users.createAccount(friendPublicAccount);
     }
@@ -42,6 +51,8 @@ public class TestUsersImpl {
         users.delete(TEST_USER);
         users.delete(TEST_FRIEND_PRIVATE);
         users.delete(TEST_FRIEND_PUBLIC);
+        users.delete(TEST_USER_TO_PUBLIC);
+        users.delete(TEST_USER_TO_PRIVATE);
     }
 
 
@@ -54,6 +65,29 @@ public class TestUsersImpl {
         assertEquals(TEST_USER, user.getUsername());
         assertEquals(TEST_PASSWORD, user.getPassword());
     }
+
+    @Test
+    public void testChangeAccountToPublic() {
+        users.updateAccountPrivacy(TEST_USER_TO_PUBLIC, "Make Account Public");
+        List<String> friends = users.listFriend(TEST_USER);
+        assertTrue(friends.isEmpty());
+
+        // validate this account could be added as a friend
+        users.updateFriend(TEST_USER, TEST_FRIEND_PUBLIC, "add");
+        friends = users.listFriend(TEST_USER);
+        assertEquals(1, friends.size());
+        assertEquals(TEST_FRIEND_PUBLIC, friends.get(0));
+
+    }
+
+    @Test
+    public void testChangeAccountToPrivate() {
+        users.updateAccountPrivacy(TEST_USER_TO_PRIVATE, "Make Account Private");
+        assertThrows(IllegalArgumentException.class,
+                () -> users.updateFriend(TEST_USER, TEST_USER_TO_PRIVATE, "add"),
+                "This user can not be added");
+    }
+
 
     @Test
     public void testAddFriends_allowed() {
