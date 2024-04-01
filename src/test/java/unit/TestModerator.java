4af68370fd2d2fc7edd39ca019bc24eb860295b1
AbdelrahmanMerdan <bodo0@DESKTOP-QMO40QH.home>
2024-03-31 21:30:43 -0400

    Removed Main method from Review

--- src/test/java/unit/TestModerator.java
+++ src/test/java/unit/TestModerator.java
@@ -1,1 +1,41 @@
+import database.UsersImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import mygaminglist.User;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestModerator {
+
+    private User moderator;
+    private User user;
+    private static final UsersImpl users = new UsersImpl();
+
+    private static final String TEST_MODERATOR = "test.user";
+    private static final String TEST_MODERATOR_PASSWORD = "test.password";
+    
+    private static final String TEST_USER = "test.user";
+    private static final String TEST_PASSWORD = "test.password";
+
+    private static final boolean TEST_IS_PRIVATE = true;
+
+
+
+    @BeforeEach
+    public void setup() {
+        user = new User(TEST_USER, TEST_PASSWORD);
+        
+        // insert a record
+        users.createAccount(user);
+
+    }
+
+    @AfterEach
+    public void clean() {
+        users.delete(TEST_USER);
+        
+    }
+}
